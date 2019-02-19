dc036607238b85fad2c8c43d9d1a544c09af91e4
Yogi <yugeshiwaram@YUGESHs-iMac.local>
2019-02-18 17:43:13 -0800

    code add

--- application.js
+++ application.js
@@ -1,1 +1,32 @@
-bank apllication
+
+let selection = prompt ("Press B: Balance, D: Deposit, W: Withdrawal, Q: Quit");
+
+  let balance = 5000;
+  let deposit= 0;
+  let withdrawal = 0;
+
+  while (selection !== "Q" && selection !== "q") {
+  switch (selection) {
+      case "B":
+      case "b":
+        alert("Your Current Balance is :" + " " + balance);
+        selection = prompt ("Press B: Balance, D: Deposit, W: Withdrawal, Q: Quit");
+
+      break;
+      case "D":
+      case "d":
+        prompt("How much would you like to deposit?");
+        selection = prompt ("Press B: Balance, D: Deposit, W: Withdrawal, Q: Quit");
+
+      break;
+      case "W":
+      case "w":
+        prompt("How much would you like to withdraw?");
+        selection = prompt ("Press B: Balance, D: Deposit, W: Withdrawal, Q: Quit");
+
+      break;
+      default :
+        selection = prompt ("Press B: Balance, D: Deposit, W: Withdrawal, Q: Quit");
+        break;
+    }
+  }
